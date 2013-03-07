90d6f218cc6578e488a26b6db87950cbb298acf7
Luís Miguel Dias Duarte <lduarte@luisduarte.net>
2013-03-07 16:16:40 +0000

    Added exercise 6. TODO: Unit Test

--- Serie1/src/pt/isel/adeetc/leic/mpd/Binder.java
+++ Serie1/src/pt/isel/adeetc/leic/mpd/Binder.java
@@ -10,18 +10,87 @@ package pt.isel.adeetc.leic.mpd;
 
 import javafx.util.Pair;
 
-import java.util.Map;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 public class Binder {
+
+    private Map<String, String> matches;
+
+    private Binder() {
+        // Doesn't make sense instantiating an Utility Class
+    }
+
     public Map<String, Object> getFields(Object o) {
-        return null;
+        Map<String, Object> result = new HashMap<String, Object>();
+        Class<?> klass = o.getClass();
+
+        for( Field field : klass.getFields() ) {
+            try {
+                result.put(field.getName(), field.get(o));
+            } catch (IllegalAccessException e) {
+                // TODO: Treat exception and rethrow, but lets ignore it for now
+                return Collections.EMPTY_MAP;
+            }
+        }
+        return result;
     }
 
     public <T> T bindTo(Class<T> klass, Iterable<Pair<String, Object>> pairs) {
-        return null;
+        T instance = null;
+        try {
+            instance = klass.newInstance();
+        } catch (Exception e) {
+            // TODO: Same as the method above
+            return null;
+        }
+
+        for( Pair<String, Object> pair : pairs) {
+            String key = pair.getKey();
+            if( matches.containsKey(key) ) {
+                key = matches.get(key);
+            }
+            Method setter = Utils.getSetter(klass, key, pair.getValue());
+            if( setter != null ) {
+                try {
+                    setter.invoke(instance, pair.getValue());
+                } catch (Exception e) {
+                    // TODO: Throw Exception
+                    return null;
+                }
+            }
+            // Lets assume that if it doesn't have a property setter, it has a field
+            try {
+                Field field = klass.getField(key);
+                int modifiers = field.getModifiers();
+                if ( Modifier.isPublic(modifiers) ) {
+                    field.set(instance, pair.getValue());
+                }
+                else {
+                    // TODO: Throw Exception
+                    return null;
+                }
+            } catch (Exception e) {
+                // TODO: Throw Exception
+                return null;
+            }
+        }
+        return instance;
     }
 
     public <TSrc, TDest> TDest map(TSrc objSrc, Class<TDest> klassDest) {
-        return null;
+        Map<String, Object> fields = getFields(objSrc);
+        Iterable<Pair<String, Object>> fieldsAsPairs = Utils.toPairs(fields);
+
+        TDest instance = bindTo(klassDest, fieldsAsPairs);
+
+        return instance;
+    }
+
+    public Binder matches(String source, String destination) {
+        matches.put(source, destination);
+        return this;
     }
 }
